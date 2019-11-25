c44c6ea485b1eac8737aecae654b751cad8ffa65
kevinlaung <kevinlaung@sina.com>
2019-11-25 09:19:04 +0800

    6 chapter

---
+++ 6/electric.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+#define RATE1 0.13230
+#define RATE2 0.15040
+#define RATE1 0.30025
+#define RATE1 0.34025
+#define BREAK1 360.0
+#define BREAK2 468.0
+#define BREAK3 720.0
+#define BASE1 (RATE1 * BREAK1)
+
+#define BASE2 (BASE1 + BASE2 * (BREAK2 - BREAK1) )
+
+#define BASE3 (BASE1 + BASE3 + (BREAK3 - BREAK2))
+
+
